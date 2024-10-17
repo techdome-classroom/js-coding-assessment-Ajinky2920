4cf06c54b1ea542adb92e850d8eca75acaf8a6e0
Ajinkya Bahiwal <ajinkya2920@gmail.com>
2024-10-17 17:47:39 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -14,7 +14,9 @@ function distinct_islands(r,c){
 }
  for (let r=0; r < rows; r++ ){
     for(let c=0; c < clos;c++){
-        if (grid[r][c]==='L')
+        if (grid[r][c]==='L'){
+            
+        }
     }
  }
 
