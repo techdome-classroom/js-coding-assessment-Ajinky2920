2cd61af3cff91bfad2fc8aab801a2ebbb2f9fb5e
Ajinkya Bahiwal <ajinkya2920@gmail.com>
2024-10-17 17:44:31 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,10 +7,10 @@ let numberofislands =0;
 function distinct_islands(r,c){
     if(r < 0 || r >= rows || c < 0 || c >=cols || grid[r][c]==='w' ) return;
 
-    distinct_islands(r+1,c)
-    distinct_islands(r-1,c)
-    distinct_islands(r,c+1)
-    distinct_islands(r,)
+    distinct_islands(r+1,c);
+    distinct_islands(r-1,c);
+    distinct_islands(r,c+1);
+    distinct_islands(r,c-1);
 }
 
 
