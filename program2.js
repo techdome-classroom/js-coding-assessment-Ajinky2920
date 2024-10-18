bcd0c048fd75114dc7bfc7eb8b68dcb1641c97b1
Ajinkya Bahiwal <ajinkya2920@gmail.com>
2024-10-18 20:39:22 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,8 +1,8 @@
 const decodeTheRing = function (s, p) {
    const match =function(SI,PI){
-if (SI===s.length && PI===p.length){
-  
-}
+      if (SI===s.length && PI===p.length){ 
+
+     }
    }
     
 
