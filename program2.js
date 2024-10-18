0f71bca443ac8489e8a2c7d21b2845b3b30ed3b7
Ajinkya Bahiwal <ajinkya2920@gmail.com>
2024-10-18 21:43:00 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,21 +2,21 @@ const decodeTheRing = function (s, p) {
    const match = function(si, pi) {
       if (si===s.length && pi===p.length){ 
       return true;
-     }
+     };
      if(p===pi.length){
       return false;
-     }
+     };
      if (p[pi]==='*'){
       return match(si,pi+1) || (si<si.length&&match(si+1,pi));
-     }
+     };
      if(p[pi]==='?'){
       return si<s.length && match(si+1,pi+1);
-     }
+     };
      if(si<s.length && s[si]===p[pi]){
       return match(si+1,pi+1);
-     }
+     };
      return true;
-   }
+   };
    return match(0,0);
   };
   console.log(decodeTheRing("hello", "h*llo"));
